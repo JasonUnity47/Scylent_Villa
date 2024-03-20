83c6405386f1b89dfc4c44b16ed23fcbb2f62126
JasonUnity47 <jasonunity47@gmail.com>
2024-03-20 23:24:23 +0800

    Idle Setup

--- Scylent Villa/Assets/Scripts/Jason/PlayerMovement.cs
+++ Scylent Villa/Assets/Scripts/Jason/PlayerMovement.cs
@@ -9,22 +9,91 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector2 movement;
 
-    private bool front;
-    private bool back;
-    private bool left;
-    private bool right;
+    public bool front = false;
+    public bool back = false;
+    public bool left = false;
+    public bool right = false;
 
     private Rigidbody2D rb;
+
+    private Animator anim;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        //Debug.Log(movement.x + "" + movement.y);
+
+        if (movement.y < 0 && !front)
+        {
+            front = true;
+            back = false;
+            left = false;
+            right = false;
+
+            //Debug.Log(1);
+
+            anim.SetBool("BackBool", back);
+            anim.SetBool("RightBool", right);
+            anim.SetBool("LeftBool", left);
+
+            anim.SetBool("FrontBool", front);
+        }
+
+        if (movement.y > 0 && !back)
+        {
+            back = true;
+            front = false;
+            left = false;
+            right = false;
+
+            //Debug.Log(2);
+
+            anim.SetBool("FrontBool", front);
+            anim.SetBool("RightBool", right);
+            anim.SetBool("LeftBool", left);
+
+            anim.SetBool("BackBool", back);
+        }
+
+        if (movement.x < 0 && !left)
+        {
+            left = true;
+            front = false;
+            back = false;
+            right = false;
+
+            //Debug.Log(3);
+
+            anim.SetBool("FrontBool", front);
+            anim.SetBool("BackBool", back);
+            anim.SetBool("RightBool", right);
+
+            anim.SetBool("LeftBool", left);
+        }
+
+        if (movement.x > 0 && !right)
+        {
+            right = true;
+            left = false;
+            front = false;
+            back = false;
+
+            //Debug.Log(4);
+
+            anim.SetBool("FrontBool", front);
+            anim.SetBool("BackBool", back);
+            anim.SetBool("LeftBool", left);
+
+            anim.SetBool("RightBool", right);
+        }
     }
 
     private void FixedUpdate()
