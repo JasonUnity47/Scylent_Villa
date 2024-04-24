12d7e1024a23c7f6ccaf10212b79cde5c5d705f2
KonJJ03 <njiajing03@gmail.com>
2024-04-24 14:18:19 +0800

    tutorial done

---
+++ Scylent Villa/Assets/Scripts/UI/TutorialText.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialText : MonoBehaviour
+{
+   
+    // Reference to the UI panel
+    public GameObject uiPanel;
+
+    // This method is called when another collider enters the trigger area
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if the collider belongs to the player
+        if (other.CompareTag("Player"))
+        {
+            // Activate the UI panel
+            uiPanel.SetActive(true);
+        }
+    }
+
+    // This method is called when another collider exits the trigger area
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        // Check if the collider belongs to the player
+        if (other.CompareTag("Player"))
+        {
+            // Deactivate the UI panel
+            uiPanel.SetActive(false);
+        }
+    }
+
+}
